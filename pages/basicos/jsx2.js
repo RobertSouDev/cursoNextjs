5c22b5249e915572791f27733ff85b3e52e90691
RobertSouDev <rogerhallks1@gmail.com>
2024-01-30 22:54:37 -0300

    first commit

---
+++ pages/basicos/jsx2.js
@@ -0,0 +1,19 @@
+export default function jsx2() {
+    const array = [
+        <l1>Wedna</l1>,
+        <l1>Robert</l1>,
+        <l1>Miguel</l1>,
+        <l1>Melissa</l1>,
+    ]
+   
+    const conteudo = (
+    <div> 
+        <h1>JSX #02 alterado</h1>
+    </div>
+)
+    return array[1]
+}
+
+// HTML, CSS E JS - tecnologias
+
+// Componetes (HTML, CSS e JS)
---
+++ pages/basicos/primeiro.js
@@ -0,0 +1,3 @@
+export default function primeiro() {
+    return "nem e a primeira vez mais"
+}
